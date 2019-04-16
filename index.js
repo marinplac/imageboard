ded0a03970f123f73753fac418eeb6410f335aa6
neshjebat <maiataraxia@yahoo.com>
2019-04-16 20:56:58 +0200

    added upload function and descriptions are now showing

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 const express = require("express");
 const app = express();
 const db = require("./utils/db");
+const s3 = require("./s3");
+const config = require("./config.json");
+
+var multer = require("multer");
+var uidSafe = require("uid-safe");
+var path = require("path");
+
+var diskStorage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function(req, file, callback) {
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    }
+});
+
+var uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152
+    }
+});
+
 app.use(require("body-parser").json());
-app.use(express.static("./public"));
+app.use(express.static("public"));
+
+app.post("/upload", uploader.single("file"), s3.upload, function(req, res) {
+    console.log("req.file: ", req.file.filename);
+
+    if (req.file) {
+        const url = config.s3Url + req.file.filename;
+        res.json({
+            success: true,
+            url
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
+});
 
 app.get("/imageboard", (req, res) => {
     db.getPics().then(({ rows }) => {
@@ -16,5 +57,5 @@ app.get("/imageboard", function(res) {
 });
 
 app.listen(8080, () => {
-    console.log("It's listening!");
+    console.log("I am listening to your blabber!");
 });
--- public/js/script.js
+++ public/js/script.js
@@ -2,14 +2,51 @@
     new Vue({
         el: "#main",
         data: {
-            images: []
+            images: [],
+            form: {
+                title: "",
+                description: "",
+                username: "",
+                file: null
+            }
         },
         mounted: function() {
             var app = this;
-            axios.get("/images").then(function(res) {
+            axios.get("/imageboard").then(function(res) {
                 app.images = res.data;
                 console.log("responded data", res.data);
             });
+        },
+        //every single funct that runs in response to an event
+        //is defined in "methods"
+        methods: {
+            handleFileChange: function(evt) {
+                console.log("file: ", evt.target.files[0]);
+                //this stores the file that was just selected
+                this.form.file = evt.target.files[0];
+                // console.log("this: ", this);
+            },
+            uploadFile: function() {
+                // formdata is used to send files to server!
+                var formData = new FormData();
+                formData.append("file", this.form.file);
+                formData.append("username", this.form.username);
+                formData.append("title", this.form.title);
+                formData.append("description", this.form.description);
+
+                axios.post("/upload", formData).then(
+                    function(res) {
+                        this.images.unshift({
+                            url: res.data.url,
+                            created_at: Date.now(),
+                            description: res.data.description,
+                            id: res.data.id,
+                            title: res.data.title,
+                            username: res.data.username
+                        });
+                    }.bind(this)
+                );
+            }
         }
     });
-});
+})();
